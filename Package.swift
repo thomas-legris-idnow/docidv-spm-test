28fafc1552a70e1257c099effce441aafd9d2506
Thomas Legris <thomas.legris.ext@idnow.io>
2025-11-12 14:10:13 +0100

    Use github release to download ReadID and FaceTec frameworks

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     ],
     // Define external dependencies (via SPM).
     dependencies: [
-        .package(url: "https://github.com/unissey/sdk-ios.git", from: "4.0.0"),
-        .package(url: "https://github.com/idnow/sunflower-sdk-ios.git", exact: "1.4.9"),
+        .package(url: "https://github.com/idnow/sunflower-sdk-ios.git", exact: "1.4.10"),
+        .package(url: "https://github.com/unissey/sdk-ios.git", from: "4.0.0")
     ],
     targets: [
         // Define a wrapper which will encapsulate every dependencies in it.
@@ -54,7 +54,7 @@ let package = Package(
             ],
             path: "sources/DocIDV-without-XS2A" // Path to another empty .swift file, needed by SPM.
         ),
-        // Define our 2 SDK binaries and their location.
+        // Define our 2 SDK internal binaries and the 2 external ones and their location.
         .binaryTarget(
             name: "DocIDV",
             url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.1.0/DocIDV.xcframework.zip",
@@ -65,49 +65,15 @@ let package = Package(
             url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.1.0/DocIDV-without-XS2A.xcframework.zip",
             checksum: "aca609143b6f5b423dd1ffd73a1d270f7ccad0eb1d07c2341072244b897769b3"
         ),
-        // Define the external dependencies imported locally.
-        .binaryTarget(
-            name: "FaceTecSDK",
-            path: "Frameworks/FaceTecSDK.xcframework"
-        ),
         .binaryTarget(
             name: "ReadID_UI",
-            path: "Frameworks/ReadID_UI.xcframework"
-        )
-    ]
-)
-
-let package = Package(
-    name: "DocIDV",
-    platforms: [.iOS(.v14)],
-    products: [
-        .library(
-            name: "DocIDV",
-            targets: [
-                "DocIDV", "FaceTecSDK", "UnisseySdk"
-            ]),
-        .library(
-            name: "DocIDV-without-XS2A",
-            targets: ["DocIDV-without-XS2A", "FaceTecSDK", "UnisseySdk"]
-        )
-    ],
-    targets: [
-        .binaryTarget(
-            name: "DocIDV",
-            path: "DocIDV/DocIDV.xcframework"
-        ),
-        .binaryTarget(
-            name: "DocIDV-without-XS2A",
-            path: "DocIDV/DocIDV-without-XS2A.xcframework"
+            url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.1.0/ReadID_UI.xcframework.zip",
+            checksum: "bb469af2c8af0f73933d747287d5f0dd31a44d9a699c9eb0a3843c3b1f7e77c2"
         ),
         .binaryTarget(
             name: "FaceTecSDK",
-            path: "ThirdParties/FaceTecSDK.xcframework"
+            url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.1.0/FaceTecSDK.xcframework.zip",
+            checksum: "bb25bd0fdaf166c9ed4ee4dd7b7e36e019a2cf62581495ee780287c02f4b59b3"
         ),
-        .binaryTarget(
-            name: "UnisseySdk",
-            path: "ThirdParties/UnisseySdk.xcframework"
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
