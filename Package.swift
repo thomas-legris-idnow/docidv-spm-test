3c626abbfbc1369df095a7a24f173ee24453ab7f
docidv-developer <docidv-developer@idnow.io>
2026-04-10 15:27:31 +0200

    Release - Update SDK to version 1.7.0

--- Package.swift
+++ Package.swift
@@ -23,27 +23,27 @@ let package = Package(
         .binaryTarget(
             name: "DocIDVCommon",
             url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.7.0/DocIDVCommon.xcframework.zip",
-            checksum: "350a2e9271dc26dee357b7e455989a426a03c09f1dbad5d51b0acd5ac2eb1427"
+            checksum: "1630bc6b4cb0292d08b4443b27e0ff6f397055032da502e4594246767d0256c1"
         ),
         .binaryTarget(
             name: "DocIDV",
             url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.7.0/DocIDV.xcframework.zip",
-            checksum: "6c2447c0ffb94c1945369c75da45653604fba942df0e09a7da21450e6433fb9b"
+            checksum: "91f1e9ad2259a6232781cc10efd30c945741e1bbddd6882f2393a2b36d40fcb9"
         ),
         .binaryTarget(
             name: "DocIDVAIModule",
             url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.7.0/DocIDVAIModule.xcframework.zip",
-            checksum: "cff715f49d40e949d4378c49573a205f85dc09491d169967543f883793e53738"
+            checksum: "86122e743fb47588d430dc317af18b91e45a4bb6f01c157f5ec47ea890a21de2"
         ),
         .binaryTarget(
             name: "DocIDVAI",
             url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.7.0/DocIDVAI.xcframework.zip",
-            checksum: "be295536612059ea48e4ab8452121d32a8768169b0b3667cadbcc7f21acb5ff5"
+            checksum: "57bff6ec238a6298d3a99cee3cbdb543e461f304aad95f64a749f88a2804c150"
         ),
         .binaryTarget(
             name: "DocIDVEIDGovernikusModule",
             url: "https://github.com/thomas-legris-idnow/docidv-spm-test/releases/download/1.7.0/DocIDVEIDGovernikusModule.xcframework.zip",
-            checksum: "54b34b67dedb4a684d8d3633a652c4e9b21a4170d849624374b41eb31d04cec2"
+            checksum: "ca40d4f28b2f36c919c11fc55bb0685b2a7bf81919430859ddfd621fae290bd5"
         ),
         .target(
             name: "DocIDV-Target-Wrapper",
